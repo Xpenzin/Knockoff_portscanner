2148e6c495ae8a797a31b187ebb658a0e4c9f5d8
Xpenzin <adarsharatz@gmail.com>
2026-07-11 22:00:32 -0400

    Update Temu_ps.py

--- Temu_ps.py
+++ Temu_ps.py
@@ -3,7 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 import sys
 
-#Use guideline for Temu_ps (Temu_PortScanner)
+#User guideline for Temu_ps (Temu_PortScanner)
 
 #Demo syntax
     #python3 Temu_ps [ip_address] [specific port/port range] [# of threads]
@@ -11,13 +11,13 @@ import sys
 #Multi-port scan 
     #Example use: python3 Temu_ps 192.168.1.4 1-500 10
     # 192.168.1.4 --> ip addr (you already know this but wtv)
-    # 1-500 --> port range between 1 through 500
-    # 10 --> The number of threads thats going to be used. Recommended is aroung  100-200, don't overdo it.
+    # 1-500 --> port range between 1 and 500
+    # 10 --> The number of threads that will be used. Recommended is around  100-200, don't overdo it.
     
 #Single-port scan
     #Example use: python3 Temu_ps 192.168.1.4 80 10
     # 192.168.1.4 --> ip addr 
-    # 80 --> Specific port that you want to lightly enumerate
+    # 80 --> Specific port that you want to enumerate lightly
     # 10 --> Same as before, the number of threads you use
 
 
@@ -38,13 +38,13 @@ def port_status(addr, port):
     finally:
         conn.close()
 
-#the number of threads that will be used during enumertaion process
+#the number of threads that will be used during the enumeration process
 num_of_threads = int(sys.argv[3])
 HOST = str(sys.argv[1])
 
 if '-' in sys.argv[2]:
 
-    #List for ports and the same host ip address in order to stisfy map requirements
+    #List for ports and the same host IP address to satisfy map requirements
     port_list = []
     Host_list = []
     PORT = str(sys.argv[2])
